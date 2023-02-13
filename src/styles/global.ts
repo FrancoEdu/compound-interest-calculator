811d11e8b8239ce8e64c064f4bd68ce74559c5dc
Franc0du <franc0edu24@gmail.com>
2023-02-13 17:16:32 -0300

    Started project

---
+++ src/styles/global.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root{
+        --background: #FFF;
+        --blue: #200F4D;
+        --blue-medium: #5429CC;
+        --blue-light: #876AD5;
+    }
+`
